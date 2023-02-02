09bb2076822c383253e9e76b54c170d8d1e2a4a5
Mark Usmanov <icemewtwo@gmail.com>
2023-02-01 18:18:50 -0700

    Update Rectangle.java

--- app/src/main/java/com/cmput301w23t00/bonkers/Rectangle.java
+++ app/src/main/java/com/cmput301w23t00/bonkers/Rectangle.java
@@ -1,7 +1,9 @@
 package com.cmput301w23t00.bonkers;
 
 class Rectangle extends Shape {
+    String color = "purple";
+
     public Rectangle(int x, int y) {
-        super(x, y)
+        super(x, y);
     }
 }
