b571ca4b2ae5c02d61b8ea4f18d1babc70f67b78
Kolby ML <31669092+KolbyML@users.noreply.github.com>
2023-02-01 17:40:12 -0700

    Added shape class

---
+++ app/src/main/java/com/cmput301w23t00/bonkers/Shape.java
@@ -0,0 +1,11 @@
+package com.cmput301w23t00.bonkers;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
