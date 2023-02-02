0ebc078cc1488c825a7596f6982d0d468d46e963
Shariq Naiyer <shariqnaiyer@gmail.com>
2023-02-01 18:26:07 -0700

    Update Trapezium.java

--- app/src/main/java/com/cmput301w23t00/bonkers/Trapezium.java
+++ app/src/main/java/com/cmput301w23t00/bonkers/Trapezium.java
@@ -1,4 +1,10 @@
 package com.cmput301w23t00.bonkers;
 
 public class Trapezium {
+<<<<<<< Updated upstream
+=======
+  public Trapezium(int x, int y, String color) {
+    super(x, y, color);
+  }
+>>>>>>> Stashed changes
 }
