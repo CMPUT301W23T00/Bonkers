e4a8ea2a665c13d41e00dde60bfab9aa03dac22f
Kolby ML <31669092+KolbyML@users.noreply.github.com>
2023-02-01 18:23:59 -0700

    Added color to circle and shape

--- app/src/main/java/com/cmput301w23t00/bonkers/Circle.java
+++ app/src/main/java/com/cmput301w23t00/bonkers/Circle.java
@@ -2,7 +2,7 @@ package com.cmput301w23t00.bonkers;
 
 public class Circle extends Shape {
 
-    Circle(int x, int y) {
-        super(x, y);
+    Circle(int x, int y, String color) {
+        super(x, y, color);
     }
 }
