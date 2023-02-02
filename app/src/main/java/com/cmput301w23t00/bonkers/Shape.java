7f90cedf057751526c108fdc8f226ab28350eb93
Kolby ML <31669092+KolbyML@users.noreply.github.com>
2023-02-01 18:26:33 -0700

    Update Shape.java

--- app/src/main/java/com/cmput301w23t00/bonkers/Shape.java
+++ app/src/main/java/com/cmput301w23t00/bonkers/Shape.java
@@ -3,7 +3,7 @@ package com.cmput301w23t00.bonkers;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color;
+    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 
     Shape(int x, int y, String color) {
         this.x = x;
