5e1fe9f202b434d5198f66cfd4e4b9028e5bf03a
Julia-Dantas <90366819+Julia-Dantas@users.noreply.github.com>
2023-02-01 17:50:59 -0700

    Added attribute
    Added attribute to Shape

--- app/src/main/java/com/cmput301w23t00/bonkers/Shape.java
+++ app/src/main/java/com/cmput301w23t00/bonkers/Shape.java
@@ -3,6 +3,7 @@ package com.cmput301w23t00.bonkers;
 public abstract class Shape {
     private int x;
     private int y;
+    String color = "blue";
 
     Shape(int x, int y) {
         this.x = x;
