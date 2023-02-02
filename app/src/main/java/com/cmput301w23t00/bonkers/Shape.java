f42649e9658bf4b1db27aae93952dae0b7042430
Ashwin Shreekumar <ashwinshreekumar@Ashwins-MacBook-Air.local>
2023-02-01 18:04:37 -0700

    Added color to shape

--- app/src/main/java/com/cmput301w23t00/bonkers/Shape.java
+++ app/src/main/java/com/cmput301w23t00/bonkers/Shape.java
@@ -3,6 +3,7 @@ package com.cmput301w23t00.bonkers;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "yellow";
 
     Shape(int x, int y) {
         this.x = x;
