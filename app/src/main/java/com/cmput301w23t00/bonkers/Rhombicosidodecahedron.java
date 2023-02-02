541c1a4f73bf0788781ff864afd3a072633735be
Dsorken <113465018+Dsorken@users.noreply.github.com>
2023-02-01 18:24:27 -0700

    fix

--- app/src/main/java/com/cmput301w23t00/bonkers/Rhombicosidodecahedron.java
+++ app/src/main/java/com/cmput301w23t00/bonkers/Rhombicosidodecahedron.java
@@ -1,6 +1,6 @@
-public class Rhombicosidodecahedron extends Shape {
-    public String color = 'blue';
+import android.graphics.drawable.shapes.Shape;
 
+public class Rhombicosidodecahedron extends Shape {
     public Rhombicosidodecahedron(int x, int y) {
         super(x, y);
     }
