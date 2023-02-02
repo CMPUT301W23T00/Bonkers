e5b89c959e25ba8f3b22a2cb7388fb46767b7893
alifbink-ualberta <alifbink@ualberta.ca>
2023-02-01 18:27:56 -0700

    Changed color.

--- app/src/main/java/com/cmput301w23t00/bonkers/Shape.java
+++ app/src/main/java/com/cmput301w23t00/bonkers/Shape.java
@@ -3,7 +3,7 @@ package com.cmput301w23t00.bonkers;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "blue";
+    private String color = "indigo";
 
     Shape(int x, int y) {
         this.x = x;
